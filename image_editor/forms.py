af3a2e3d8771b273ed94e9965926a1573fb04aa2
Mohamed osama <134468542+mohamedo22@users.noreply.github.com>
2024-06-23 23:20:53 +0300

    Add files via upload

---
+++ image_editor/forms.py
@@ -0,0 +1,56 @@
+from django import forms
+
+class TextForm(forms.Form):
+    text1 = forms.CharField(
+        max_length=100,
+        required=False,
+        widget=forms.TextInput(attrs={'placeholder': 'مقدمة الشكر'})
+    )
+    text2 = forms.CharField(
+        max_length=100,
+        required=False,
+        widget=forms.TextInput(attrs={'placeholder': 'اسم الممنوح له الشهاده او التكريم (او مجموعة اسماء)'})
+    )
+    text3 = forms.CharField(
+        max_length=100,
+        required=False,
+        widget=forms.TextInput(attrs={'placeholder': 'عبارات الشكر'})
+    )
+    text4 = forms.CharField(
+        max_length=100,
+        required=False,
+        widget=forms.TextInput(attrs={'placeholder': 'الخاتمة'})
+    )
+    text5 = forms.CharField(
+        max_length=100,
+        required=False,
+        widget=forms.TextInput(attrs={'placeholder': 'أسم المسؤول'})
+    )
+    text6 = forms.CharField(
+        max_length=100,
+        required=False,
+        widget=forms.TextInput(attrs={'placeholder': 'أسم المسؤول'})
+    )
+    text7 = forms.CharField(
+        max_length=100,
+        required=False,
+        widget=forms.TextInput(attrs={'placeholder': 'المسؤول'})
+    )
+    text8 = forms.CharField(
+        max_length=100,
+        required=False,
+        widget=forms.TextInput(attrs={'placeholder': 'المسؤول'})
+    )
+
+    class Meta:
+        labels = {
+            'text1': '',
+            'text2': '',
+            'text3': '',
+            'text4': '',
+            'text5': '',
+            'text6': '',
+            'text7': '',
+            'text8': '',
+        }
+        label_suffix = ''
