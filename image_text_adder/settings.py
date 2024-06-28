59894296d39675f64d954a51f70bdd0e4935dd98
mohamedo22 <oooomohamed421@gmail.com>
2024-06-28 21:39:36 +0300

    update

--- image_text_adder/settings.py
+++ image_text_adder/settings.py
@@ -122,3 +122,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CELERY_BROKER_URL = 'redis://:password@hostname:port/0'
+CELERY_RESULT_BACKEND = 'redis://:password@hostname:port/0'
