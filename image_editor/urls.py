df0d38929e54c6860b012c68cbbc11aa5ad2196a
mohamedo22 <oooomohamed421@gmail.com>
2024-06-29 02:07:56 +0300

    update

--- image_editor/urls.py
+++ image_editor/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('download_pdf/', views.download_pdf, name='download_pdf'),
+    # path('download_pdf/', views.download_pdf, name='download_pdf'),
     path('download_from_home/', views.download_from_home, name='downloadFromHome'),
 ]
